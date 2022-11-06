38b680100bc0d654b54b804fdb876288d93842d0
rk-pdx <91846253+rk-pdx@users.noreply.github.com>
2022-11-06 13:24:53 -0800

    committing all hw4 files

--- hw4/02-sessions.js
+++ hw4/02-sessions.js
@@ -1,16 +1,37 @@
+
 const express = require('express');
 const session = require('express-session');
-const app = express();
-const port = process.env.PORT || 5001;
+const app     = express();
+const port    = process.env.PORT || 5001;
 
-// Add your code here
 
-// Use the express-session module
-app.use(/** ... */);
+app.use(session({secret: 'secret_key'}));
+
 
 app.get('/', (req, res) => {
-  // Add your code here
+  if (req.session.first_time) {
+    req.session.routes.push('/');
+    res.send(`Navigate to any route.`);
+  }
+  else {
+    req.session.first_time = true;
+    req.session.routes = ['/'];
+    res.send(`<h1>Welcome to http://localhost:${port}</h1>`);
+  }
+  res.end();
 });
+
+
+app.get('/*', (req, res) => {
+  if (!req.session.routes || req.url === '/favicon.ico') res.redirect('/');
+  else {
+    res.write(`Currently on route: ${req.url}\nPreviously visited: `);
+    req.session.routes.forEach((route) => res.write(`${route} `));
+    req.session.routes.push(req.url);
+    res.end();
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
